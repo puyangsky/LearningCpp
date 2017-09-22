f5f7b9529916a0b489186b2de3f82211278e2f19
puyangsky <puyangsky@pku.edu.cn>
2017-09-23 01:23:38 +0800

    init

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by puyangsky on 17/9/23.
+//
+
+#include <iostream>
+#include "Test.h"
+
+namespace fuck {
+    void Test::init() {
+        std::cout<< "init..."<<std::endl;
+    }
+}
